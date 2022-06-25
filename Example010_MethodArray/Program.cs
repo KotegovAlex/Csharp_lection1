42749f25d3c0bd352109c50b325715e6b5c6c573
Kotegov_Alexander <s.kotegov2010@yandex.ru>
2022-06-25 15:21:37 +0500

    add Example010 Method Array

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 12, 31, 4, 18, 15, 16, 17, 18 }; // объявление массива
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прерывание цикла
+    }
+    index++;
+}
